458db834658cda881e3536f56f1b15d5dd68fcdc
AndreiFlorian <56733066+Andrei-Florian@users.noreply.github.com>
2020-06-15 14:00:17 +0100

    Update secrets.h

--- secrets.h
+++ secrets.h
@@ -3,7 +3,7 @@
 #define DEVELOP_TIME 10000  // 10 seconds
 
 // Everything that has to be edited goes here
-#define DEVICE_ID 1             // the ID of the device, needed when more devices are being connected to distinguish between them
+#define DEVICE_ID "John Doyle"  // the ID of the device, needed when more devices are being connected to distinguish between them
 #define SECRET_BROKER ""        // the iot hub name with .azure-devices.net at the end
 #define SECRET_DEVICEID ""      // the name of the device
 #define MODE DEVELOP_TIME       // change to RELEASE_TIME when deploying
